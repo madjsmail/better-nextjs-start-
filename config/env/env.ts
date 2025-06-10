13dcfdc7a53a05a14366b2b3c111b027689c29d3
madjidsmail <madjidsmail1998@gmail.com>
2025-06-10 02:59:47 +0100

    feat: :bookmark: init /base app

---
+++ config/env/env.ts
@@ -0,0 +1,21 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z } from "zod";
+
+expand(config());
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production"]).default("development"),
+    // NEXTAUTH_URL: z.string().url(),
+    // NEXTAUTH_SECRET: z.string()
+  },
+  onValidationError: (error) => {
+    console.error("❌ Invalid environment variables:", error);
+    process.exit(1);
+  },
+  emptyStringAsUndefined: true,
+  // eslint-disable-next-line n/no-process-env
+  experimental__runtimeEnv: process.env,
+});
---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+// import type { NextConfig } from "next";
+
+import createJiti from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+jiti("./config/env/env.ts");
+
+const nextConfig = {
+  output: "standalone",
+
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  experimental: {
+    typedRoutes: true,
+  },
+};
+
+export default nextConfig;
